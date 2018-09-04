774910e227264606f2e262e42ad92063397e9885
fengpengyuan <fengpengyuan@live.com>
2018-09-04 17:56:38 +0800

    1

--- src/views/userCenter/index.vue
+++ src/views/userCenter/index.vue
@@ -1,8 +1,24 @@
 <template>
-    
+    <div>
+        <Header></Header>
+        <h1>这里是用户中心</h1>
+    </div>
 </template>
 <script>
+import Header from '@/views/layout/Header'
 export default {
-    
+    name:'index',
+    components: {
+        Header
+    },
+    data(){
+        return {
+
+        }
+    },
+    created(){},
+    methods:{
+        
+    }
 }
 </script>
