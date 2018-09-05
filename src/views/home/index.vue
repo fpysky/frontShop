e2601bc785a3483c3710d908e42757af6aa15c66
fengpengyuan <fengpengyuan@live.com>
2018-09-05 17:48:02 +0800

    1

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,5 +1,10 @@
 <template>
-    <Header></Header>
+    <div>
+        <Header></Header>
+        <div class="home">
+            <div class="home-carousel"></div>
+        </div>
+    </div>
 </template>
 <script>
 import Header from '@/views/layout/Header'
@@ -19,3 +24,14 @@ export default {
     }
 }
 </script>
+<style scoped>
+.home {
+    background: #f4f4f4;
+    position: relative;
+    overflow: hidden;
+}
+.home-carousel {
+    width: 100%;
+    height: 480px;
+}
+</style>
