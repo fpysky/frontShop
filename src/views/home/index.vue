ba98f1a71c9ad0960e55a02493f3cbbe58ecc220
fengpengyuan <fengpengyuan@live.com>
2018-09-11 10:02:14 +0800

    1

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -2,7 +2,13 @@
     <div>
         <Header></Header>
         <div class="home">
-            <div class="home-carousel"></div>
+            <div class="home-carousel">
+                <el-carousel height="480px" :interval="5000" arrow="always">
+                    <el-carousel-item v-for="item in 4" :key="item">
+                    <h3>{{ item }}</h3>
+                    </el-carousel-item>
+                </el-carousel>
+            </div>
         </div>
     </div>
 </template>
@@ -34,4 +40,19 @@ export default {
     width: 100%;
     height: 480px;
 }
+.el-carousel__item h3 {
+    color: #475669;
+    font-size: 18px;
+    opacity: 0.75;
+    line-height: 300px;
+    margin: 0; 
+  }
+  
+  .el-carousel__item:nth-child(2n) {
+    background-color: #99a9bf;
+  }
+  
+  .el-carousel__item:nth-child(2n+1) {
+    background-color: #d3dce6;
+  }
 </style>
